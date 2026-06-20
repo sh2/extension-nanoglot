33b7bafc80bdd2ba1d64ddf70b676c1413e5f99c
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2026-06-20 13:44:04 +0900

    feat: update ESLint configuration and dependencies for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
 
 
 export default [
+  {
+    ignores: ["extension/lib/**"]
+  },
   pluginJs.configs.recommended,
   {
     languageOptions: {
@@ -13,9 +17,36 @@ export default [
     }
   },
   {
+    plugins: {
+      "@stylistic": stylistic
+    },
     rules: {
-      "quotes": ["error", "double", { "avoidEscape": true }],
-      "semi": ["error", "always"]
+      "@stylistic/quotes": ["error", "double", { "avoidEscape": true }],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/no-trailing-spaces": "error",
+      "@stylistic/padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "block-like" },
+        { blankLine: "always", prev: "block-like", next: "*" },
+        { blankLine: "always", prev: "*", next: "multiline-expression" },
+        { blankLine: "always", prev: "multiline-expression", next: "*" },
+        { blankLine: "always", prev: "*", next: "multiline-const" },
+        { blankLine: "always", prev: "multiline-const", next: "*" },
+        { blankLine: "always", prev: "*", next: "multiline-let" },
+        { blankLine: "always", prev: "multiline-let", next: "*" },
+        { blankLine: "always", prev: "*", next: "multiline-var" },
+        { blankLine: "always", prev: "multiline-var", next: "*" }
+      ],
+      "@stylistic/lines-around-comment": [
+        "error",
+        {
+          beforeLineComment: true,
+          allowBlockStart: true,
+          allowObjectStart: true,
+          allowArrayStart: true,
+          allowClassStart: true
+        }
+      ]
     }
   }
 ];
